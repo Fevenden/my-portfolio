06a3e966508f8bccadd1f60db24944960f115cad
Freedom Evenden <ftevenden1@gmail.com>
2019-12-05 10:03:19 -1000

    implemented JS, increased accessibility, finished layout design

--- index.js
+++ index.js
@@ -1,18 +1,40 @@
+//handles scroll and scroll animation
 function scrollTo(destination) {
     $('html, body').animate({
         scrollTop: $(destination).offset().top
      }, 1000);
 }
 
-function handleClickContact() {
-$('.contactLink').click(function(e) {
-    e.preventDefault()
-    scrollTo("header")
-})
+//when user clicks a nav button screen will scroll to specified location.
+function handleNavClick() {
+  $('#aboutMeLink').click(function(e) {
+    e.preventDefault();
+    scrollTo('.aboutMe')
+  });
+  $('#myProjectsLink').click(function(e) {
+    e.preventDefault();
+    scrollTo('.projects-box')
+  });
+  $('.toTop').click(function(e) {
+    e.preventDefault();
+  });
+  $('#skyrimLink').click(function(e) {
+    e.preventDefault();
+    scrollTo('#project1');
+  });
+}
+
+//displays dropdown menu when user taps hamburger
+function handleHamburgerCLick() {
+  $('.hamburger').click(function(e){
+    e.preventDefault();
+    $('.hiddenMenu').toggleClass('expose');
+  })
 }
 
 function init() {
-    handleClickContact()
+  handleNavClick();
+  handleHamburgerCLick();
 }
 
 $(init)
