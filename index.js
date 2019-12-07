6de4a097fb94d734df5e5287f956fe1949bcb9de
Freedom Evenden <ftevenden1@gmail.com>
2019-12-06 20:07:12 -1000

    added required changes for resubmit

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 //handles scroll and scroll animation
 function scrollTo(destination) {
-    $('html, body').animate({
-        scrollTop: $(destination).offset().top
-     }, 1000);
+  $('html, body').animate({
+      scrollTop: $(destination).offset().top
+    }, 1000);
 }
 
 //when user clicks a nav button screen will scroll to specified location.
@@ -11,28 +11,41 @@ function handleNavClick() {
     e.preventDefault();
     scrollTo('.aboutMe')
   });
+
   $('#myProjectsLink').click(function(e) {
     e.preventDefault();
     scrollTo('.projects-box')
   });
+
   $('.toTop').click(function(e) {
     e.preventDefault();
   });
+
   $('#skyrimLink').click(function(e) {
     e.preventDefault();
     scrollTo('#project1');
   });
 }
 
-//displays dropdown menu when user taps hamburger
+//displays or hides dropdown menu when user taps hamburger
 function handleHamburgerCLick() {
-  $('.hamburger').click(function(e){
+  $('.hamburger').click(function(e) {
     e.preventDefault();
     $('.hiddenMenu').toggleClass('expose');
   })
 }
 
+// when about me clicked will display or hide about me section
+function handleAboutMeClick() {
+  $('#aboutMeLink2').click(function(e) {
+    e.preventDefault();
+    $('.aboutMe').toggleClass('hide');
+    $('main').toggleClass('center');
+  });
+}
+
 function init() {
+  handleAboutMeClick()
   handleNavClick();
   handleHamburgerCLick();
 }
