f5199057bb1249f8e68e6d097b79f3d77ba265f8
fteve96 <ftevenden1@gmail.com>
2020-05-06 21:13:45 -0800

    added BuildTech. Cleaned up some of the js

--- index.js
+++ index.js
@@ -2,51 +2,56 @@
 function scrollTo(destination) {
   $('html, body').animate({
       scrollTop: $(destination).offset().top
-    }, 1000);
+    }, 1000)
 }
 
 //when user clicks a nav button screen will scroll to specified location.
 function handleNavClick() {
-  $('#aboutMeLink').click(function(e) {
-    e.preventDefault();
+  $('#aboutMeLink').click(e => {
+    e.preventDefault()
     scrollTo('.aboutMe')
-  });
+  })
 
-  $('#myProjectsLink').click(function(e) {
-    e.preventDefault();
+  $('#myProjectsLink').click(e => {
+    e.preventDefault()
     scrollTo('.projects-box')
-  });
+  })
 
-  $('.toTop').click(function(e) {
-    e.preventDefault();
-  });
+  $('.toTop').click(e => {
+    e.preventDefault()
+  })
 
-  $('#skyrimLink').click(function(e) {
-    e.preventDefault();
-    scrollTo('#project1');
-  });
+  $('#skyrimLink').click(e => {
+    e.preventDefault()
+    scrollTo('#project1')
+  })
 
-  $('#spaceyLink').click(function(e) {
-    e.preventDefault();
-    scrollTo('#project2');
+  $('#spaceyLink').click(e => {
+    e.preventDefault()
+    scrollTo('#project2')
+  })
+
+  $('#buildTechLink').click(e => {
+    e.preventDefault()
+    scrollTo('#project3')
   })
 }
 
 //displays or hides dropdown menu when user taps hamburger
 function handleHamburgerCLick() {
-  $('.hamburger').click(function(e) {
-    e.preventDefault();
-    $('.hiddenMenu').toggleClass('expose');
+  $('.hamburger').click(e => {
+    e.preventDefault()
+    $('.hiddenMenu').toggleClass('expose')
   })
 }
 
 // when about me clicked will display or hide about me section
 function handleAboutMeClick() {
-  $('#aboutMeLink2').click(function(e) {
-    e.preventDefault();
-    $('.aboutMe').toggleClass('hide');
-    $('main').toggleClass('center');
-  });
+  $('#aboutMeLink2').click(e => {
+    e.preventDefault()
+    $('.aboutMe').toggleClass('hide')
+    $('main').toggleClass('center')
+  })
 }
 
 function init() {
