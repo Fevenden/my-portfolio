b3c937153f39acef8c465c35790811cc465183c0
f3ve <free.eve@pm.me>
2020-07-24 14:50:58 -0700

    cleaned up code

--- index.js
+++ index.js
@@ -1,68 +1,71 @@
 //handles scroll and scroll animation
 function scrollTo(destination) {
-  $('html, body').animate({
-      scrollTop: $(destination).offset().top
-    }, 1000)
+  $('html, body').animate(
+    {
+      scrollTop: $(destination).offset().top,
+    },
+    1000
+  );
 }
 
 //when user clicks a nav button screen will scroll to specified location.
 function handleNavClick() {
-  $('#aboutMeLink').click(e => {
-    e.preventDefault()
-    scrollTo('.aboutMe')
-  })
+  $('#aboutMeLink').click((e) => {
+    e.preventDefault();
+    scrollTo('.aboutMe');
+  });
 
-  $('#myProjectsLink').click(e => {
-    e.preventDefault()
-    scrollTo('.projects-box')
-  })
+  $('#myProjectsLink').click((e) => {
+    e.preventDefault();
+    scrollTo('.projects-box');
+  });
 
-  $('.toTop').click(e => {
-    e.preventDefault()
-  })
+  $('.toTop').click((e) => {
+    e.preventDefault();
+  });
 
-  $('#skyrimLink').click(e => {
-    e.preventDefault()
-    scrollTo('#project1')
-  })
+  $('#skyrimLink').click((e) => {
+    e.preventDefault();
+    scrollTo('#project1');
+  });
 
-  $('#spaceyLink').click(e => {
-    e.preventDefault()
-    scrollTo('#project2')
-  })
+  $('#spaceyLink').click((e) => {
+    e.preventDefault();
+    scrollTo('#project2');
+  });
 
-  $('#buildTechLink').click(e => {
-    e.preventDefault()
-    scrollTo('#project3')
-  })
+  $('#buildTechLink').click((e) => {
+    e.preventDefault();
+    scrollTo('#project3');
+  });
 
-  $('#blogiumLink').click(e => {
-    e.preventDefault()
-    scrollTo('#project4')
-  })
+  $('#blogiumLink').click((e) => {
+    e.preventDefault();
+    scrollTo('#project4');
+  });
 }
 
 //displays or hides dropdown menu when user taps hamburger
 function handleHamburgerCLick() {
-  $('.hamburger').click(e => {
-    e.preventDefault()
-    $('.hiddenMenu').toggleClass('expose')
-  })
+  $('.hamburger').click((e) => {
+    e.preventDefault();
+    $('.hiddenMenu').toggleClass('expose');
+  });
 }
 
 // when about me clicked will display or hide about me section
 function handleAboutMeClick() {
-  $('#aboutMeLink2').click(e => {
-    e.preventDefault()
-    $('.aboutMe').toggleClass('hide')
-    $('main').toggleClass('center')
-  })
+  $('#aboutMeLink2').click((e) => {
+    e.preventDefault();
+    $('.aboutMe').toggleClass('hide');
+    $('main').toggleClass('center');
+  });
 }
 
 function init() {
-  handleAboutMeClick()
+  handleAboutMeClick();
   handleNavClick();
   handleHamburgerCLick();
 }
 
-$(init)
+$(init);
